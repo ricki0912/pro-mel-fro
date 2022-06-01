a813a7b1d89d452c90204d5d8c95165eb7e74771
unknown <jc.condor.sistemas@gmail.com>
2022-06-01 18:22:57 -0500

    Actualizado al 01/06/2022 a 18:22 por jul

--- src/app/module-si/client-view/pages/inline-edit/inline-edit.component.ts
+++ src/app/module-si/client-view/pages/inline-edit/inline-edit.component.ts
@@ -15,8 +15,8 @@ import { filter } from 'rxjs/operators';
       </mat-form-field>
 
       <div class="actions">
-        <button mat-button type="button" color="primary" (click)="onCancel()">CANCEL</button>
-        <button mat-button type="submit" color="primary">SAVE</button>
+        <button mat-button type="button" color="primary" (click)="onCancel()">Cancelar</button>
+        <button mat-button type="submit" color="primary">Listo</button>
       </div>
     </form>
   `
