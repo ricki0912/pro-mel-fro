236f53fb4ecb7664953f023ccfa6d7eb7f9e70fc
P <ricki.0912@gmail.com>
2023-01-21 09:35:24 -0500

    cambios modificado 20/01/2022

--- src/app/module-si/client-view/pages/annual-summary/table-register/table-register.component.ts
+++ src/app/module-si/client-view/pages/annual-summary/table-register/table-register.component.ts
@@ -8,13 +8,22 @@ import { AnnualResumeDetails } from 'src/app/interfaces/annual-resume-details';
 })
 export class TableRegisterComponent {
   @Input() isEditable:boolean=false
-   dataSourceM: AnnualResumeDetails[] = [];
+  @Input() hasModified:boolean=false
+  dataSourceM: AnnualResumeDetails[] = [];
    dataSourceA: AnnualResumeDetails[] = [];
+
+  
    
+
+   //
+   private _dataSourceMOld: AnnualResumeDetails[] = [];
+   private _dataSourceAOld: AnnualResumeDetails[] = [];
+
+
    arDescription:string=''
    
    @Output() onSave = new EventEmitter<AnnualResumeDetails[]>();
-   public setSave = ()=> this.onSave.emit([...this.dataSourceA, ...this.dataSourceM])
+   public setSave = ()=> this.onSave.emit([ ...this.dataSourceM, ...this.dataSourceA])
     @Output() onArDescription=new EventEmitter<string>()
     public setArDescription=()=>this.onArDescription.emit(this.arDescription)
 
@@ -24,6 +33,9 @@ export class TableRegisterComponent {
     this.dataSourceM = val.filter(e=>((e.ardMonth || 0)>=1 && (e.ardMonth ||0)<=12));
     this.dataSourceA=val.filter(e=>(e.ardMonth ||0) >=13 )
     
+
+    /*this._dataSourceAOld=val.filter(e=>(e.ardMonth ||0) >=13 )
+    this._dataSourceMOld=val.filter(e=>((e.ardMonth || 0)>=1 && (e.ardMonth ||0)<=12));*/
   /*  if(this.bussines?.bussId && this._period?.prdsId){
       this.getDataAnnualResume(this.bussines.bussId, this._period.prdsId)
     }*/
@@ -48,6 +60,91 @@ export class TableRegisterComponent {
    }
   }
   findMonth = (id: number) => MONTHS.find((e) => e.id == id);
+
+  saveRow(element:any){
+    element.isEdit = !element.isEdit
+    this.hasModified=true
+    /*let indexMOld=this._dataSourceMOld.findIndex(e=>e.ardMonth==element.ardMonth);
+    
+    let selectedRowOld=this._dataSourceMOld[indexMOld];
+
+
+    console.log(element, selectedRowOld)
+    
+    if(this.compare(element, selectedRowOld)){
+      console.log("Modificado con exito")
+    }    */
+
+    
+    
+
+    
+  }
+
+
+  compare( x:any, y:any ) {
+    if ( x === y ) return true;
+    // if both x and y are null or undefined and exactly the same
+
+    if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+    // if they are not strictly equal, they both need to be Objects
+
+    if ( x.constructor !== y.constructor ) return false;
+    // they must have the exact same prototype chain, the closest we can do is
+    // test there constructor.
+
+    for ( var p in x ) {
+        if ( ! x.hasOwnProperty( p ) ) continue;
+        // other properties were tested using x.constructor === y.constructor
+
+       if ( ! y.hasOwnProperty( p ) ) return false;
+       // allows to compare x[ p ] and y[ p ] when set to undefined
+
+       if ( x[p] === y[p] ) continue;
+       // if they have the same strict value or identity then they are equal
+
+       if ( typeof( x[ p ] ) !== "object" ) return false;
+       // Numbers, Strings, Functions, Booleans must be strictly equal
+
+       if ( ! this.compare( x[ p ],  y[ p ] ) ) return false;
+       // Objects and Arrays must be tested recursively
+   }
+
+   for ( p in y ) {
+      if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+      // allows x[ p ] to be set to undefined
+    }
+    return true;
+}
+
+
+  areEqual=(dataSourceA: AnnualResumeDetails[],dataSourceM: AnnualResumeDetails[])=>{
+    let state=false;
+    for (let i:number=0; i<dataSourceM.length;i++){
+        /*if(dataSourceM[i] as AnnualResumeDetails!=this._dataSourceMOld[i] as AnnualResumeDetails){
+          return true;
+        }  */
+      
+      let object=dataSourceM[i] as any;
+        let objectOld=this._dataSourceMOld[i] as any;
+      for (let key in object) {
+        if(object[key]!=objectOld[key]){
+          return true
+          console.log("HOlaaa")
+        }
+      /*
+        if (Object.prototype.hasOwnProperty.call(key, key)) {
+          const element = object[key];
+          
+        }*/
+      }
+    }
+    
+
+    return state
+    //return (JSON.stringify(dataSourceA)==JSON.stringify(this._dataSourceAOld) || JSON.stringify(dataSourceM)==JSON.stringify(this._dataSourceMOld) ) 
+   
+  }
 }
 
 
